08cb994b7d64ec37126256c2a09e4df11de05ba2
Jonathan Chan <johnkchan252@gmail.com>
2019-06-14 01:28:29 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ function getRepositories() {
   req.send();
 }
 
-function getCommits(el) {
+function displayCommits(el) {
   const name = el.dataset.repo;
   const req = new XMLHttpRequest();
   req.addEventListener('load', showCommits);
